0be65a05641a01e5a98c53280ad26e681f8217f0
Gzim <gzimasanni@gmail.com>
2021-04-21 01:02:27 +0200

    Add submit form

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' }
+
+  onInputChange = () => {
+
+  }
+
+  render(){
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input 
+            type="text" 
+            value={this.state.term} 
+            onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
