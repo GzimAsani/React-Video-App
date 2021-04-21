d6158d42363745ea557a152b165ceae755424987
Gzim <gzimasanni@gmail.com>
2021-04-21 15:55:31 +0200

    Add VideoDetail component which will show the seletcted video from the user/state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = {videos: [], selectedVideo: null }
@@ -16,13 +17,14 @@ class App extends React.Component {
   }
 
   onVideoSelect = video => {
-    console.log('This video is coming from app', video)
+    this.setState({ selectedVideo: video })
   }
 
   render(){
     return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onTermSubmit} />
+      <VideoDetail video={this.state.selectedVideo} />
       <VideoList 
         onVideoSelect={this.onVideoSelect} 
         videos={this.state.videos} 
