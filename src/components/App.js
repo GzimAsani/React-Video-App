690edf0aa7f572027aed6309bf752ed3efa3d8d8
Gzim <gzimasanni@gmail.com>
2021-04-21 16:35:58 +0200

    Add default video selection

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,20 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
   state = {videos: [], selectedVideo: null }
   
+  componentDidMount() {
+    this.onTermSubmit('cars')
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({videos: response.data.items})
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   }
 
   onVideoSelect = video => {
