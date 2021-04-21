a4df4283123e7e4c5703c4d7c7b89dde3e1a31d7
Gzim <gzimasanni@gmail.com>
2021-04-21 03:29:11 +0200

    Add VideoLIst component

---
+++ src/components/VideoItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoItem = (props) => {
+  return <div>VideoItem</div>
+};
+
+export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem'
 
-const VideoList = ({videos}) => {
- return <div>{videos.length}</div>
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+   return <VideoItem />
+  })
+ return <div>{renderedList}</div>
 };
 
 export default VideoList;
