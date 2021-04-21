8dadb61b747c1dd0a352d4d142facb1442965c05
Gzim <gzimasanni@gmail.com>
2021-04-21 15:45:02 +0200

    Add connection between selected user video

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-  state = {videos: [] }
+  state = {videos: [], selectedVideo: null }
   
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -15,11 +15,18 @@ class App extends React.Component {
     this.setState({videos: response.data.items})
   }
 
+  onVideoSelect = video => {
+    console.log('This video is coming from app', video)
+  }
+
   render(){
     return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onTermSubmit} />
-      <VideoList videos={this.state.videos} />
+      <VideoList 
+        onVideoSelect={this.onVideoSelect} 
+        videos={this.state.videos} 
+      />
       </div>
     
     )
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return( 
-    <div className="item video-item">
+    <div onClick={() => onVideoSelect(video)} className="item video-item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
